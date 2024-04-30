09842ab34b3eac055dc7c75dd8fea44109b737ab
Heder David <hedermd6@gmail.com>
2024-04-30 15:13:49 -0300

    FILAMED

--- src/main/kotlin/com/example/filamed2/HelloApplication.kt
+++ src/main/kotlin/com/example/filamed2/HelloApplication.kt
@@ -3,13 +3,18 @@ package com.example.filamed2
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.stage.Stage
 
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("home-view.fxml"))
         val scene = Scene(fxmlLoader.load())
-        stage.title = "Hello!"
+        //val iconPath = "../imgs/filamed-logo.png"
+        //val iconStream = javaClass.classLoader.getResourceAsStream(iconPath)
+        //val icon = Image(iconStream)
+        //stage.icons.add(icon)
+        stage.title = "FILAMED"
         stage.scene = scene
         stage.show()
     }
