e31f177e8f5819e26ad9754b913ac44bb755de87
Heder David <hedermd6@gmail.com>
2024-05-01 14:13:10 -0300

    criação do controller da tela de prioridades

--- src/main/kotlin/com/example/filamed2/PrioridadeController.kt
+++ src/main/kotlin/com/example/filamed2/PrioridadeController.kt
@@ -1,4 +1,12 @@
 package com.example.filamed2
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
 class PrioridadeController {
+
+
 }
