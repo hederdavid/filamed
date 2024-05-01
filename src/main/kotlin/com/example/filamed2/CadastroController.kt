4bbd1b09027f583655880811a784becd9c8223e7
Heder David <hedermd6@gmail.com>
2024-05-01 14:12:38 -0300

    adição de evento pra chamar proxima tela

--- src/main/kotlin/com/example/filamed2/CadastroController.kt
+++ src/main/kotlin/com/example/filamed2/CadastroController.kt
@@ -1,4 +1,19 @@
 package com.example.filamed2
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
 class CadastroController {
+    fun carregarTelaPrioridade(event: ActionEvent) {
+        val root: Parent = FXMLLoader.load(javaClass.getResource("prioridade-view.fxml"))
+        val scene = Scene(root)
+
+        val stage = Stage()
+        stage.title = "Prioridade"
+        stage.scene = scene
+        stage.show()
+    }
 }
