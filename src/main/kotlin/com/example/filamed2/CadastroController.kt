1775ba9bd65bb8380e150acae3d42c47896dc9de
Heder David <hedermd6@gmail.com>
2024-05-02 09:05:24 -0300

    salvar dados do cadastro funcionando

--- src/main/kotlin/com/example/filamed2/CadastroController.kt
+++ src/main/kotlin/com/example/filamed2/CadastroController.kt
@@ -1,13 +1,37 @@
 package com.example.filamed2
 
 import javafx.event.ActionEvent
+import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.TextField
 import javafx.stage.Stage
 
 class CadastroController {
-    fun carregarTelaPrioridade(event: ActionEvent) {
+    lateinit var nomeField: TextField
+    lateinit var cpfField: TextField
+    lateinit var sexoField: TextField
+    lateinit var dataNascimentoField: TextField
+    lateinit var relatoQueixasSintomasField: TextField
+    fun salvarDadosEChamarProximaTela(event: ActionEvent) {
+            val nome: String = nomeField.text
+            val cpf: String = cpfField.text
+            val sexo: String = sexoField.text
+            val dataNascimento: String = dataNascimentoField.text
+            val relato: String = relatoQueixasSintomasField.text
+
+            // Adicione aqui a lógica de verificação do nome de usuário e senha
+            println("Nome: $nome")
+            println("CPF: $cpf")
+            println("Sexo: $sexo")
+            println("Data de Nascimento: $dataNascimento")
+            println("Relato: $relato")
+            carregarTelaPrioridade()
+    }
+
+    private fun carregarTelaPrioridade() {
         val root: Parent = FXMLLoader.load(javaClass.getResource("prioridade-view.fxml"))
         val scene = Scene(root)
 
